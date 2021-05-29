c9c04d678e306c3e80e3c16b6ddabc01a9f54fbb
unknown <you@example.com>
2021-05-29 02:35:04 +0100

    adding animation to the hero section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
 import Basement from "./components/basement/Basement";
 import Footer from "./components/footer/Footer";
@@ -7,10 +8,47 @@ import Merch from "./components/merch/Merch";
 import Navbar from "./components/navbar/Navbar";
 import Resource from "./components/resources/Resource";
 import "./normalize.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
+  const appRef = useRef(null);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    const element = appRef.current;
+
+    tl.from(element.querySelector(".heading"), {
+      opacity: 0,
+      x: 200,
+      duration: 0.8,
+    })
+      .from(element.querySelector(".hero-items"), {
+        opacity: 0,
+        y: -200,
+        duration: 0.5,
+      })
+      .from(element.querySelector(".hero-wrapper2"), {
+        opacity: 0,
+        y: 200,
+        duration: 0.5,
+      });
+  }, []);
+
+  useEffect(() => {
+    const element = appRef.current;
+    tl.from(element.querySelectorAll(".inter-box"), {
+      opacity: 0,
+      y: 200,
+      duration: 2,
+      stagger: 0.4,
+    });
+  }, []);
+
   return (
-    <div className="app">
+    <div className="app" ref={appRef}>
       <Navbar />
       <Hero />
       <Inter />
